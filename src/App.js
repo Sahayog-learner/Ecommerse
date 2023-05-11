2dad3f8595b9828da12f964e3acfabb3520e7151
Sahayog-leraner <sahayogtimalsina07@gamil.com>
2023-05-11 12:58:10 +0545

    added header component with links to home ,shop page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route,Routes} from 'react-router-dom';
+import {  Route,Routes} from 'react-router-dom';
 import React from 'react';
 import './App.css';
 
 import Homepage from './pages/homepage.components/homepage.component';
 import ShopPage from './pages/homepage.components/shop/shop.component';
+import Header from './components/menu-item/header component/header.component';
 
 
 
 function App() {
   return (
     <div>
-     <BrowserRouter>
-      <Routes>
-        <Route exact path = '/' Component={Homepage} />
-        <Route path='/shop' Component={ShopPage} />
-      </Routes>
-     </BrowserRouter>
+      <Header/>
+        
+          <Routes>
+            <Route exact path = '/' Component={Homepage} />
+            <Route path='/shop' Component={ShopPage} />
+          </Routes>
+        
     </div>
   
   );
